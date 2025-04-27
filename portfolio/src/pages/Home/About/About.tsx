6eb52b7b965bf96e3af779e8cfa2f7696fb1d39c
douglasvcv <douglasvitor1233@gmail.com>
2025-04-27 00:38:31 -0300

    feat: building navBar component

--- portfolio/src/pages/Home/About/About.tsx
+++ portfolio/src/pages/Home/About/About.tsx
@@ -1,9 +1,22 @@
+import { Container, styled } from "@mui/material"
+import theme from "../../../theme";
+
 const About = ()=> {
   
-
+  const StyledAbout = styled("div")(() => ({
+    backgroundColor: theme.palette.primary.main,
+    height: "100vh",
+    margin: "0px",
+    display:"flex",
+    alignItems:"center"
+}))
     return (
       <>
-       About
+       <StyledAbout>
+        <Container>
+          
+        </Container>
+       </StyledAbout>
       </>
     )
   }
--- portfolio/src/pages/NavBar/NavBar.tsx
+++ portfolio/src/pages/NavBar/NavBar.tsx
@@ -1,13 +1,23 @@
-import { AppBar, MenuItem, styled, Toolbar } from "@mui/material"
+import { AppBar, Button, Menu, MenuItem, styled, Toolbar } from "@mui/material"
+import MenuIcon from '@mui/icons-material/Menu';
+import theme from "../../theme";
+import { useState } from "react";
 
 const NavBar = () => {
 
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+    const open = Boolean(anchorEl)
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget)
+    }
 
+    const handleClose = ()=>{
+        setAnchorEl(null)
+    }
     const StyledToobar = styled(Toolbar)(() => ({
         display: "flex",
         alignItems: "center",
-        justifyContent: "space-evenly",
-        fontSize:""
+        justifyContent: "space-evenly"
     }))
 
     return (
@@ -18,6 +28,27 @@ const NavBar = () => {
                     <MenuItem>Skills</MenuItem>
                     <MenuItem>Projects</MenuItem>
                 </StyledToobar>
+                <Button
+                    color={theme.palette.primary.contrastText}
+                    id="fade-button"
+                    aria-controls={open ? "fade-menu" : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? "true" : undefined}
+                    onClick={handleClick}
+                >
+                    <MenuIcon />
+                </Button>
+                <Menu
+                    id="fade-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+
+                >
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem onClick={handleClose}>Logout</MenuItem>
+                </Menu>
             </AppBar>
         </>
     )
